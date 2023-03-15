27d01d6c904c8afdf9468e45ccdf6b65a17c969c
James Ressler <jressler7@gmail.com>
2023-03-15 14:46:08 -0700

    fix navbar collapse toggle

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,29 @@
 let div = document.createElement("div");
 
 div.innerHTML = `
-<nav class="navbar navbar-expand-lg bg-body-tertiary nav-list">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="../html/index.html">James Ressler</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../html/index.html">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../html/experience.html">Experience</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../html/projects.html">Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="../html/contact.html">Contact</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+<div class="container-fluid">
+  <a class="navbar-brand" href="../html/index.html">James Ressler</a>
+  <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="../html/index.html">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../html/experience.html">Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../html/projects.html">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../html/contact.html">Contact</a>
+      </li>
+    </ul>
   </div>
+</div>
 </nav>
 `;
 
