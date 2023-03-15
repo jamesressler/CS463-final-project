b0014338e37612cad305ec54cf9520c2e929d0e0
James Ressler <jressler7@gmail.com>
2023-03-15 14:08:15 -0700

    fix navbar responsiveness

--- js/navbar.js
+++ js/navbar.js
@@ -1,40 +1,34 @@
-let nav = document.createElement("nav");
-let ul = document.createElement("ul");
-let about = document.createElement("li");
-let experience = document.createElement("li");
-let projects = document.createElement("li");
-let contact = document.createElement("li");
-let aboutLink = document.createElement("a");
-let experienceLink = document.createElement("a");
-let projectsLink = document.createElement("a");
-let contactLink = document.createElement("a");
+let div = document.createElement("div");
 
-ul.setAttribute("class", "nav-list");
+div.innerHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary nav-list">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../html/index.html">James Ressler</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../html/index.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../html/experience.html">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../html/projects.html">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../html/contact.html">Contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+`;
 
-aboutLink.setAttribute("class", "nav-link");
-aboutLink.setAttribute("href", "../html/index.html");
-aboutLink.textContent = "About";
-
-experienceLink.setAttribute("class", "nav-link");
-experienceLink.setAttribute("href", "../html/experience.html");
-experienceLink.textContent = "Experience";
-
-projectsLink.setAttribute("class", "nav-link");
-projectsLink.setAttribute("href", "../html/projects.html");
-projectsLink.textContent = "Projects";
-
-contactLink.setAttribute("class", "nav-link");
-contactLink.setAttribute("href", "../html/contact.html");
-contactLink.textContent = "Contact";
-
-about.append(aboutLink);
-experience.append(experienceLink);
-projects.append(projectsLink);
-contact.append(contactLink);
-
-ul.append(about);
-ul.append(experience);
-ul.append(projects);
-ul.append(contact);
-nav.append(ul);
-document.querySelector("body").prepend(nav);
+document.querySelector("body").prepend(div);
